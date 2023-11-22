09bfe8bb87043f0a2c43c905962c2d8681fa0d14
ademiralves <cgademir@gmail.com>
2023-11-21 21:19:51 -0300

    NewLinguaap

--- README.md
+++
@@ -1,1 +0,0 @@
-# This is my personal portfolio in order to show my projects.
--- script.js
+++ script.js
@@ -6,28 +6,3 @@ $(document).ready(function(){
     });
   });
 });
-
-function openNav() {
-  document.getElementById("mySidebar").style.width = "80%";
-  document.getElementById("main").style.marginLeft = "350px";
-}
-
-function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
-}
-
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
